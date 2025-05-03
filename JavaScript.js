e3e3edb3165f782e77b64b47cc06bdae244b0ff5
Layanavv <2021wb86018@wilp.bits-pilani.ac.in>
2025-05-04 00:03:45 +0530

    Create JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+echo "console.log('Version 1 - JavaScript');" > JavaScript.js
+git add JavaScript.js
+git commit -m "Add JavaScript.js version 1"
+
+# Version 2
+echo "// Version 2" >> JavaScript.js
+git commit -am "Update JavaScript.js to version 2"
